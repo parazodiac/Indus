2a0a346971b64566c2e0a37b1d9a28efaa8d093c
Avi Srivastava <asrivastava@cs.stonybrook.edu>
2021-03-24 18:34:58 -0400

    clearing things

--- src/configs.rs
+++
@@ -1,1 +0,0 @@
-pub const NUM_SAMPLES: usize = 1_000_000;
--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,7 @@ extern crate sce;
 use clap::{App, Arg, SubCommand};
 use std::error::Error;
 
-mod configs;
-mod gibbs;
-mod links;
-mod multimodal;
 mod spatial;
-mod unify;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::new("indus")
@@ -62,55 +57,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                         .help("path to the output file."),
                 ),
         )
-        .subcommand(
-            SubCommand::with_name("gamma")
-                .about("A subcommand to generate gamma fields.")
-                .arg(
-                    Arg::with_name("ipaths")
-                        .long("ipaths")
-                        .short("i")
-                        .takes_value(true)
-                        .required(true)
-                        .multiple(true)
-                        .help("path to the parent folders of matrices."),
-                )
-                .arg(
-                    Arg::with_name("links")
-                        .long("links")
-                        .short("l")
-                        .takes_value(true)
-                        .required(true)
-                        .help("path to the file with feature links."),
-                )
-                .arg(
-                    Arg::with_name("anchors")
-                        .long("anchors")
-                        .short("a")
-                        .takes_value(true)
-                        .help("path to the file with cellular barcode anchors."),
-                )
-                .arg(
-                    Arg::with_name("microclusters")
-                        .long("microclusters")
-                        .short("m")
-                        .takes_value(true)
-                        .help("path to the file with microclusters of pivot assay."),
-                )
-                .arg(
-                    Arg::with_name("output")
-                        .long("output")
-                        .short("o")
-                        .takes_value(true)
-                        .required(true)
-                        .help("path to the output path file."),
-                ),
-        )
         .get_matches();
     pretty_env_logger::init_timed();
-
-    if let Some(sub_m) = matches.subcommand_matches("gamma") {
-        unify::callback(&sub_m)?
-    }
 
     if let Some(sub_m) = matches.subcommand_matches("autocorr") {
         spatial::callback(&sub_m)?
